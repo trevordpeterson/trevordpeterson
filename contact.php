47668c812b0c4df164d1ef9426b6846aabd7f0a0
trevordpeterson <61563865+trevordpeterson@users.noreply.github.com>
2020-02-27 20:39:37 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,5 +1,15 @@
 <?php
-if($_POST["message"]) {
-    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+
+if($_POST["submit"]) {
+    $recipient="bal247@cornell.edu";
+    $subject="Website Form Submission";
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["your message"];
+
+    $mailBody="Name: $sender\nEmail: $email\n\n$your message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$email>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
-?>
